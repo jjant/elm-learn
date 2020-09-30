299a7b8c5ce938d4dc8c6ff6b565a45878fd61f5
jjant <jantonielli@itba.edu.ar>
2020-09-30 19:40:10 -0300

    1.0.9 change

--- src/Learn.elm
+++ src/Learn.elm
@@ -1,7 +1,4 @@
--- effect module Learn where { subscription = MySub } exposing (myVal)
-
-
-module Learn exposing (myVal)
+effect module Learn where { subscription = MySub } exposing (myVal)
 
 import Platform exposing (Task)
 import Task
@@ -45,53 +42,76 @@ myVal =
 --         }
 -----
 -- SUBSCRIPTIONS
--- mySub : (Int -> msg) -> Sub msg
--- mySub msg =
---     subscription (MySub msg)
--- type MySub msg
---     = MySub (Int -> msg)
--- subMap : (a -> b) -> MySub a -> MySub b
--- subMap func sub =
---     case sub of
---         MySub tagger ->
---             MySub (func << tagger)
--- -- EFFECT MANAGER
--- type alias State =
---     { sentNumber : Bool }
--- -- NOTE: used in onEffects
--- --
--- init : Task Never State
--- init =
---     Task.succeed { sentNumber = False }
--- onEffects : Platform.Router msg Int -> List (MySub msg) -> State -> Task Never State
--- onEffects router subs state =
---     -- case subs of
---     --    [] -> Task.succeed state
---     --    (sub1::subs) ->
---     --     (if not state.sentNumber then
---     --         sendNumber router sub1
---     --     else
---     --         Task.succeed state)
---     --             |> Task.andThen (\newState ->
---     --                 List.foldl
---     --             )
---     List.foldl
---         (\sub bigTask ->
---             bigTask
---                 |> Task.andThen
---                     (\modifiedState ->
---                         if not modifiedState.sentNumber then
---                             sendNumber router sub
---                         else
---                             Task.succeed state
---                     )
---         )
---         (Task.succeed state)
---         subs
--- sendNumber : Platform.Router msg x -> MySub msg -> Task Never State
--- sendNumber router (MySub tagger) =
---     Platform.sendToApp router (tagger 42)
---         |> Task.map (\_ -> { sentNumber = True })
--- onSelfMsg : Platform.Router msg Int -> Int -> State -> Task Never State
--- onSelfMsg router newTime state =
---     Task.succeed state
+
+
+mySub : (Int -> msg) -> Sub msg
+mySub msg =
+    subscription (MySub msg)
+
+
+type MySub msg
+    = MySub (Int -> msg)
+
+
+subMap : (a -> b) -> MySub a -> MySub b
+subMap func sub =
+    case sub of
+        MySub tagger ->
+            MySub (func << tagger)
+
+
+
+-- EFFECT MANAGER
+
+
+type alias State =
+    { sentNumber : Bool }
+
+
+
+-- NOTE: used in onEffects
+--
+
+
+init : Task Never State
+init =
+    Task.succeed { sentNumber = False }
+
+
+onEffects : Platform.Router msg Int -> List (MySub msg) -> State -> Task Never State
+onEffects router subs state =
+    -- case subs of
+    --    [] -> Task.succeed state
+    --    (sub1::subs) ->
+    --     (if not state.sentNumber then
+    --         sendNumber router sub1
+    --     else
+    --         Task.succeed state)
+    --             |> Task.andThen (\newState ->
+    --                 List.foldl
+    --             )
+    List.foldl
+        (\sub bigTask ->
+            bigTask
+                |> Task.andThen
+                    (\modifiedState ->
+                        if not modifiedState.sentNumber then
+                            sendNumber router sub
+
+                        else
+                            Task.succeed state
+                    )
+        )
+        (Task.succeed state)
+        subs
+
+
+sendNumber : Platform.Router msg x -> MySub msg -> Task Never State
+sendNumber router (MySub tagger) =
+    Platform.sendToApp router (tagger 42)
+        |> Task.map (\_ -> { sentNumber = True })
+
+
+onSelfMsg : Platform.Router msg Int -> Int -> State -> Task Never State
+onSelfMsg router newTime state =
+    Task.succeed state
